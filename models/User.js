0029e2fd68e61d6e163b31f6656b17b0d4cf3227
Aaroh Mankad <aarohmankad@gmail.com>
2018-10-24 19:08:13 -0700

    add functionality to hash password and compare hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
 /**
  * User model
  * @type {Schema}
  */
-var User = new mongoose.Schema({
+const User = new mongoose.Schema({
   createdDate: {
     type: Date,
     default: Date.now,
@@ -27,6 +28,39 @@ var User = new mongoose.Schema({
     required: true,
   },
 });
+
+User.pre('save', function(next) {
+  let user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(12, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+User.methods.comparePassword = function(password, callback) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
 
 // Allow us to export model to other files (e.x. routes)
 module.exports = mongoose.model('User', User);
